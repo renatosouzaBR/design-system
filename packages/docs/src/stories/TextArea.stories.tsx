3f0d6f0e567bb3f761b6f8284b9dea0130e0f1e4
Renato Souza <natoicm@gmail.com>
2023-04-06 17:34:39 -0300

    feat: add TextArea component

---
+++ packages/docs/src/stories/TextArea.stories.tsx
@@ -0,0 +1,37 @@
+import { StoryObj, Meta } from '@storybook/react'
+import { Text, TextArea, TextAreaProps, Box } from '@renatosouzaBR-ui/react'
+
+export default {
+  title: 'Form/Text Area',
+  component: TextArea,
+  args: {},
+  decorators: [
+    (StoryObj) => {
+      return (
+        <Box
+          as="label"
+          css={{
+            display: 'flex',
+            flexDirection: 'column',
+            gap: 8,
+          }}
+        >
+          <Text size="sm">Observations</Text>
+          <StoryObj />
+        </Box>
+      )
+    },
+  ],
+} as Meta<TextAreaProps>
+
+export const Primary: StoryObj<TextAreaProps> = {
+  args: {
+    placeholder: 'Type your observations',
+  },
+}
+
+export const Disabled: StoryObj<TextAreaProps> = {
+  args: {
+    disabled: true,
+  },
+}
---
+++ packages/react/src/components/TextArea.tsx
@@ -0,0 +1,34 @@
+import { ComponentProps } from 'react'
+import { styled } from '../styles'
+
+export const TextArea = styled('textarea', {
+  borderRadius: 6,
+  padding: '$3 $4',
+  backgroundColor: '$gray900',
+  border: '2px solid $gray900',
+  boxSizing: 'border-box',
+  minWidth: 80,
+
+  fontFamily: '$default',
+  fontSize: '$sm',
+  fontWeight: '$regular',
+  color: '$white',
+
+  resize: 'vertical',
+  outline: 0,
+
+  '&:focus': {
+    borderColor: '$ignite500',
+  },
+
+  '&::placeholder': {
+    color: '$gray400',
+  },
+
+  '&:disabled': {
+    opacity: 0.5,
+    cursor: 'not-allowed',
+  },
+})
+
+export interface TextAreaProps extends ComponentProps<typeof TextArea> {}
